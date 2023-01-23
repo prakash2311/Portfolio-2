ec00278fb55efd989ab8345ab6e1f9dd28d70206
Prakash2311 <prakashdevta@gmail.com>
2023-01-23 20:26:04 +0200

    updating services file

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./services.css";
 
 const Services = () => {
+    const [toggleState, setToggleState ] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
     return (
         <section className="services section" id="services">
             <h2 className="section__title">Services</h2>
@@ -12,16 +18,15 @@ const Services = () => {
                 <div>
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">Product <br /> Designer</h3>
-                    <h3 className="services__title"></h3>
                 </div>
 
-                <span className="services__button">View More
+                <span className="services__button" onClick={() => toggleTab(1)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "active-modal services__modal " : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i  onClick ={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
                         <h3 className="services__modal-title">Product Designer</h3>
                         <p className="services__modal-description">Services with more than 2 years of experience
@@ -63,16 +68,15 @@ const Services = () => {
                 <div>
                     <i className="uil uil-arrow services__icon"></i>
                     <h3 className="services__title">Ui/Ux <br/> Designer</h3>
-                    <h3 className="services__title"></h3>
                 </div>
 
-                <span className="services__button">View More
+                <span className="services__button" onClick={() => toggleTab(2)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
                         <h3 className="services__modal-title">Ui/Ux Designer</h3>
                         <p className="services__modal-description">Services with more than 2 years of experience
@@ -114,16 +118,15 @@ const Services = () => {
                 <div>
                     <i className="uil uil-edit services__icon"></i>
                     <h3 className="services__title">Visual <br/> Designer</h3>
-                    <h3 className="services__title"></h3>
                 </div>
 
-                <span className="services__button">View More
+                <span className="services__button" onClick={() => toggleTab(3)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 3 ? "services__modal active-modal " : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
                         <h3 className="services__modal-title">Visual Designer</h3>
                         <p className="services__modal-description">Services with more than 2 years of experience
