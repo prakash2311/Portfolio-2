42e220f5c3b7889c602417e5af3fe4e0fbd47cd3
Prakash2311 <prakashdevta@gmail.com>
2024-06-08 11:36:15 +0300

    adding .pdf

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpeg"
-import  CV from "../../assets/prakashcv"
+import  CV from "../../assets/prakashcv.pdf"
 import Info from "./Info"
 
 const About = () => {
